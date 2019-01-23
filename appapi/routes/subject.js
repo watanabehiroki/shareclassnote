341b3781bed11029af2b0419237492ccab637ab4
watanabehiroki <kbc15a20@stu.kawahara.ac.jp>
2019-01-23 15:43:32 +0900

    client側のapi修正

--- appapi/routes/subject.js
+++ appapi/routes/subject.js
@@ -74,7 +74,7 @@ router.post('/updatesubject',function (req,res) {
    try{
        sql = "update subject set  name = "+ name+", color ="+ color +
            ", updateday = now() where id = "+id+";";
-       connection.query(sql,function(errrows){
+       connection.query(sql,function(err,rows){
            if(err){
                result = {
                    result:'err',
--- appapi/routes/userlogin.js
+++ appapi/routes/userlogin.js
@@ -2,49 +2,91 @@ var express = require('express');
 var router = express.Router();
 const mysql = require('mysql');
 const sqlconf = require('../config/sqlconfig');
-const connection = mysql.createConnection(sqlconf);
+const connection = mysql.createConnection(sqlconf.mysql);
+const randommodule = require('./module/random');
 
 router.post('/clientlogin',function(req,res){
-    let userid = req.body.userid;
-    let password = req.body.password;
-    var responcedata;
-    var sql = "select * from clientuser left outer join " +
-        "clientapisession on clientuser.userid = clientapisession.userid where " +
-        "clientuser.userid = "+userid+";";
-    try {
-        var sqldata;
-        connection.query(sql, function (err, rows) {
-            if(err){
-                responcedata = {
-                    result: 'err',
-                    message: 'sqlerr'
-                }
-            } else {
-                sqldata = rows;
-                if(sqldata.length < 0){
-                    responcedata = {
-                        result:'success',
-                        login:'fail'
-                    }
-                }else{
-                    responcedata ={
+   let userid = req.body.userid;
+   let password = req.body.password;
+   var responcedata={
+       result:'',
+       sessionid:'',
+       message:''
+   };//返却値
+    var sql;
+   try{
+       sql = 'select user.userid, session.endday as sessionday, session.sessionid from clientuser as user' +
+           ' left outer join clientapisession as session on session.userid = user.userid where user.userid ="'+userid
+           +' "and user.password="'+password+'"and user.delflg = false;';
+       connection.query(sql,function(err,rows){
+          var sqldata = rows;
+          //login判定
+          if(sqldata.length > 0){
+              var sessionlist;
+              var whileflg;
+              if(sqldata.sessionid == undefined || sqldata.sessionid == null|| sqldata.sessionid == ''){
+                  connection.query('select sessionid from clientapisession;',function(err,rows){
+                     if(err){
+                     responcedata.result='err';
+                     return res.json(responcedata);
+                     } else{
+                         sessionlist = rows;
+                         whileflg = true;
 
-                        result:'success',
-                        login:'success',
-                        logindata:sqldata[0]
-                    }
-                }
-            }
+                         while (whileflg) {
+                             responcedata.sessionid = 'drthgjhkjl';
+                             if(sessionlist.length < 1){
+                                 whileflg = false;
+                             }else{
+                                 for (var i = 0; i < refressionlist.length; i++) {
+                                     if (i == sessionlist.length-1 && sessionlist[i].sessionid != responcedata.sessionid) {
+                                         whileflg = false;
+                                     }
+                                 }
 
-        });
-    }catch(e){
-        responcedata = {
-            result:'err',
-            message:'tryerr'
-        }
-    }
-    return res.json(responcedata);
+                             }
+                         }
+
+                         sql = 'insert into clientapisession(userid,endday,sessionid) ' +
+                             'values ("'+userid+'",DATE_ADD(now(),INTERVAL 120 DAY),"'+responcedata.sessionid+'");';
+                         console.log(sql);
+                         connection.query(sql,function (err,rows) {
+                             if(!err){
+                                 responcedata.result='success';
+                                 return res.json(responcedata);
+                             }else{
+                                 return res.json({
+                                     result:'err',
+                                     message:'sqlerr'
+                                 })
+                             }
+                         });
+
+                     }
+                  });
+              }else{
+                  //既に一回以上はログインしている場合
+                  responcedata.result='success';
+                  responcedata.sessionid = sqldata.sessionid;
+                  return res.json(responcedata);
+              }
+          }else{
+              //一致しない場合
+              responcedata = {
+                  result:'success',
+                  login:'fail'
+              }
+              return res.json(responcedata);
+          }
+       });
+   }catch (e){
+       return res.json({
+           result:'err',
+           message:undefined
+       });
+   }
 });
+
 router.post('/adminlogin',function(req,res){
     let email = req.body.email;
     let password = req.body.password;
@@ -77,14 +119,17 @@ router.post('/adminlogin',function(req,res){
                    }
                }
            }
+
+            return res.json(resultdata);
         });
     }catch(e){
         resultdata = {
             result:'err',
             message:'tryerr'
         }
+
+        return res.json(resultdata);
     }
-    return res.json(resultdata);
 });
 
 module.exports = router;
